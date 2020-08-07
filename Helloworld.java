561667bf1ab0fe179b343625086165d9e34af90c
GangaPraveen <gangapraveen.mgc@gmail.com>
2020-08-07 11:20:33 -0500

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		for (int i=0;i<10;i++) {
-			System.out.println("Hi praveen"+i);
+			System.out.println("Hi Ganga"+i);
 		}
 
 	}
